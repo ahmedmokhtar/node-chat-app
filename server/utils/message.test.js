c61f910b8f434ef4270383076aa1fa025de1f808
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-10-26 20:19:03 +0200

    Finished n-9-7: added generateMessage utility function

---
+++ server/utils/message.test.js
@@ -0,0 +1,17 @@
+const expect = require('expect')
+
+const {generateMessage} = require('./message')
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        const from = 'Admin'
+        const text = 'A message from Admin'
+        const message = generateMessage(from, text)
+
+        // expect(message.from).toBe(from)
+        // expect(message.text).toBe(text)
+        expect(message).toHaveProperty('from', from)
+        expect(message).toHaveProperty('text', text)
+        expect(typeof message.createdAt).toBe('number')
+    })
+})

83056b73f84a8f0cfebc8b8aa73e4f9dd00c094d
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-10-27 08:36:49 +0200

    Finished n-9-11: added geolocation feature using geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect')
 
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -13,6 +13,18 @@ describe('generateMessage', () => {
         expect(message).toHaveProperty('from', from)
         expect(message).toHaveProperty('text', text)
         expect(message).toMatchObject({from, text})
+        expect(typeof message.createdAt).toBe('number')
+    })
+})
+
+describe('generateLocationMessage', () => {
+    it('should generate correct geolocation object', () => {
+        const message = generateLocationMessage('Admin', 1, 2)
+
+        expect(message).toMatchObject({
+            from: 'Admin',
+            url: 'https://google.com/maps?q=1,2'
+        })
         expect(typeof message.createdAt).toBe('number')
     })
 })
